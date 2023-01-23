95f31d5f573860a06b43cf597e691f4fed87f3aa
SerKinast <122878913+SerKinast@users.noreply.github.com>
2023-01-23 05:05:17 +0300

    copy test files

---
+++ copy_test.py
@@ -0,0 +1,12 @@
+
+the_start = 1
+while the_start == 1:
+    more = input('Если хотите начать сначала, напишите "Да": ')
+    if more == 'Да' or 'да':
+        the_start = 1
+    else:
+        the_start = 0
+
+# p = os.path.basename(\python\python5lesson)
+# print(type(os.path.abspath()))
+# print(os.listdir(\Обучение\python\python5lesson))
